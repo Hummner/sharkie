4a28b6b74537d4d5d972b9d7795fde78d7774102
Hummner <bence.cservenyak@gmail.com>
2025-05-12 07:25:27 +0200

    first enemy and light

---
+++ classes/light.class.js
@@ -0,0 +1,11 @@
+class Light extends MovableObject {
+x = 0;
+y = 0;
+width = 780;
+height = 400;
+
+constructor() {
+    super().loadImage("img/3. Background/Layers/1. Light/COMPLETO.png");
+
+    }
+}
--- classes/world.class.js
+++ classes/world.class.js
@@ -4,10 +4,32 @@ class World {
         new Chicken(),
         new Chicken(),
         new Chicken()
-];
+    ];
+    light = new Light();
+
+    ctx;
+    canvas;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas
+        this.draw();
+    }
 
 
-draw() {
+    draw() {
+        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.charachter.img, this.charachter.x, this.charachter.y, this.charachter.width, this.charachter.height);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
+        })
+        this.ctx.drawImage(this.light.img, this.light.x, this.light.y, this.light.width, this.light.height)
 
-}
+
+
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+    }
 }
